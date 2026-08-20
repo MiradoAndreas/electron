c200a021895b5c0b4a7db75ed2bcf53385136322
Mirado <vonjiniainamirado@gmail.com>
2026-08-20 17:20:07 +0300

    file system integration

--- src/renderer/src/components/note-preview.tsx
+++ src/renderer/src/components/note-preview.tsx
@@ -8,7 +8,6 @@ export type NotePreviewProps = NoteInfo & {
 
 export const NotePreview = ({
   title,
-  content,
   lastEditTime,
   isActive = false,
   className,
--- src/renderer/src/store/index.ts
+++ src/renderer/src/store/index.ts
@@ -1,8 +1,17 @@
 import { NoteInfo } from '@shared/models'
 import { atom } from 'jotai'
-import { notesMock } from '@/store/mocks'
+import { unwrap } from 'jotai/utils'
 
-export const notesAtom = atom<NoteInfo[]>(notesMock)
+const loadNotes = async () => {
+  const notes = await window.context.getNotes()
+
+  // sort them by most recentyl edited
+  return notes.sort((a, b) => b.lastEditTime - a.lastEditTime)
+}
+
+const notesAtomAsync = atom<NoteInfo[] | Promise<NoteInfo[]>>(loadNotes())
+
+export const notesAtom = unwrap(notesAtomAsync, (prev) => prev)
 
 export const selectedNoteIndexAtom = atom<number | null>(null)
 
@@ -10,7 +19,7 @@ export const selectedNoteAtom = atom((get) => {
   const notes = get(notesAtom)
   const selectedNoteIndex = get(selectedNoteIndexAtom)
 
-  if (selectedNoteIndex == null) return null
+  if (selectedNoteIndex == null || !notes) return null
   const selectedNote = notes[selectedNoteIndex]
 
   return {
@@ -21,6 +30,8 @@ export const selectedNoteAtom = atom((get) => {
 
 export const createEmptyNoteAtom = atom(null, (get, set) => {
   const notes = get(notesAtom)
+
+  if (!notes) return
 
   const title = `Note ${notes.length + 1}`
 
@@ -39,7 +50,7 @@ export const deleteNoteAtom = atom(null, (get, set) => {
 
   const selectedNote = get(selectedNoteAtom)
 
-  if (!selectedNote) return
+  if (!selectedNote || !notes) return
   set(
     notesAtom,
     notes.filter((note) => note.title !== selectedNote.title)
